d163dd29ee097c36de395c06d79b0746ee6eafbc
guigui <g.desjonqueres@gmail.com>
2019-05-20 12:40:02 -0700

    Fix security issue

--- src/base.js
+++ src/base.js
@@ -3,9 +3,9 @@ import firebase from 'firebase/app'
 import 'firebase/database'
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyB-_40I36CoBxkjJunmdEbfQWficUoaQ5A",
-  authDomain: "recipe-app-b9332.firebaseapp.com",
-  databaseURL: "https://recipe-app-b9332.firebaseio.com"
+  apiKey: require('./firebase.key'),
+  authDomain: 'recipe-app-b9332.firebaseapp.com',
+  databaseURL: 'https://recipe-app-b9332.firebaseio.com'
 })
 
 const base = Rebase.createClass(firebaseApp.database())
